ed4ee8302a48aec0d57b15059b4adea3be4093b9
Venkanna-1009 <venkannachowdary2000@gmail.com>
2025-11-14 12:28:19 +0530

    data transformation is staged

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
+#from collections.abc import MutableMapping
 from NetworkSecurity.components.Data_Ingestion import DataIngestion
 from NetworkSecurity.components.Data_Validation import DataValidation
+from NetworkSecurity.components.Data_Transformation import DataTransformation
 from NetworkSecurity.exception.exception import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
 from NetworkSecurity.entity.config_entity import TrainingPipelineConfig
-from NetworkSecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from NetworkSecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+
 
 
 import os
@@ -24,7 +27,11 @@ if __name__ == "__main__":
         logging.info(f"initiate data validation config")
         datavalidationartifact = data_validation.initiate_data_validation()
         print(datavalidationartifact)
-
+        datatransformationconfig = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(datavalidationartifact, datatransformationconfig)
+        logging.info(f"initiate data transformation config")
+        datatransformationartifact = data_transformation.initiate_data_transformation()
+        print(datatransformationartifact)
         
     except Exception as e:
         raise NetworkSecurityException(e, sys)
